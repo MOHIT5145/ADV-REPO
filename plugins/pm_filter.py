5740334c8501d3bb1d61548819eb444f5d179f27
Wrrohit788 <154300370+Wrrohit788@users.noreply.github.com>
2025-05-27 21:24:35 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2674,7 +2674,7 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
             
             
         else:
-            await reply_msg.edit_text("⚠️ Your message is too long. Please enter a shorter query (less than 100 characters).")
+            await reply_msg.edit_text("⚠️ WARNING DON'TSHARE LINKS. <br> REQUEST MOVIES @MOTUMOVIES")
             return
     else:
         message = msg.message.reply_to_message  # msg will be callback query
