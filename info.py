4680b89789e6a5586de66c19d9502f6d630f2e53
MOHIT5145 <uvrohit004@gmail.com>
2025-05-29 22:14:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,7 +88,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+czIO66maaiBkODA9')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+czIO66maaiBkODA9')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'AARLINK') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'khushibots') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/wrrohit02')
 
 # True Or False
