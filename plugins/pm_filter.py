d5f849483da65c81b2342115abf55311efb37151
MOHIT5145 <uvrohit004@gmail.com>
2025-05-29 16:41:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2647,7 +2647,7 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
                     else:
                         # Second LibGen fallback attempt
                         try:
-                                await reply_msg.edit_text(f"🔍 SEARCHING ...")
+                              await reply_msg.edit_text(f" SEARCHING'{name}'...")
                             results = await libgen_search(name)
                             
                             if results:
