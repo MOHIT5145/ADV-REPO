c70aa54e8720d8f43f7ac93f80bfc3bc6e71c934
Wrrohit788 <154300370+Wrrohit788@users.noreply.github.com>
2025-07-02 21:01:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -65,8 +65,8 @@ MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', True)) # Set True or F
 
 # If Multiple Database Is True Then Fill All Three Below Database Uri Else You Will Get Error.
 O_DB_URI = environ.get('O_DB_URI', "mongodb+srv://micipe8509:1YMGz2Idfm5l4s35@cluster0.akxuslx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # This Db Is For Other Data Store
-F_DB_URI = environ.get('F_DB_URI', "mongodb+srv://nokes23103:dCsbMAAovONF4GSb@cluster0.x8hketi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # This Db Is For File Data Store
-S_DB_URI = environ.get('S_DB_URI', "mongodb+srv://jenoso4498:E9AivZpS3PiSmEUJ@cluster0.ubyg3yz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # This Db is for File Data Store When First Db Is Going To Be Full.
+F_DB_URI = environ.get('F_DB_URI', "mongodb+srv://jenoso4498:E9AivZpS3PiSmEUJ@cluster0.ubyg3yz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # This Db Is For File Data Store
+S_DB_URI = environ.get('S_DB_URI', "mongodb+srv://nokes23103:dCsbMAAovONF4GSb@cluster0.x8hketi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # This Db is for File Data Store When First Db Is Going To Be Full.
 
 
 # Premium And Referal Settings
